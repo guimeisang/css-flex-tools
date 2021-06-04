52d6dbf2bc69c21101c847e336ca7fe6e2477f16
guimeisang <guimeisang@users.noreply.github.com>
2021-06-04 20:18:20 +0800

    add: mobx

--- src/views/Detail/index.tsx
+++ src/views/Detail/index.tsx
@@ -1,15 +1,22 @@
+import { observable } from 'mobx'
 import * as React from 'react'
 import Hello from './components/hello'
-import { Input } from './components/input'
+import { Input, InputState } from './components/input'
+import { observer } from 'mobx-react-lite'
 
-const Detail: React.FunctionComponent = (props) => {
+class DetailState {
+  @observable
+  input: InputState = new InputState()
+}
+
+const Detail: React.FunctionComponent = observer((props) => {
   return (
     <>
       <h1>详情页</h1>
       <Hello compiler="ts" framework="react" />
-      <Input state="" />
+      <Input state={DetailState} />
     </>
   )
-}
+})
 
 export default Detail
