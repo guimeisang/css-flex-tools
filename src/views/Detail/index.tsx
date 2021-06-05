13c21b1fec2a67eb2a42d45faa43ee4ff6d15934
guimeisang <guimeisang@users.noreply.github.com>
2021-06-05 11:39:44 +0800

    add: 修改下

--- src/views/Detail/index.tsx
+++ src/views/Detail/index.tsx
@@ -1,20 +1,13 @@
 import { observable } from 'mobx'
 import * as React from 'react'
 import Hello from './components/hello'
-import { Input, InputState } from './components/input'
 import { observer } from 'mobx-react-lite'
-
-class DetailState {
-  @observable
-  input: InputState = new InputState()
-}
 
 const Detail: React.FunctionComponent = observer((props) => {
   return (
     <>
       <h1>详情页</h1>
       <Hello compiler="ts" framework="react" />
-      <Input state={DetailState} />
     </>
   )
 })
